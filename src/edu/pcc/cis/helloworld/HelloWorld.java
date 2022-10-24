b8b0c628fae239289a738ab6c4a2ae907acdbdab
M_Gould <michael.gould@pcc.edu>
2022-10-23 19:24:31 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * created by Michael Gould 8.27.2022
  */
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+    }
 }
