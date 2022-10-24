c3c6381680a6ecd0a5d9286b077c870f4d2e5065
M_Gould <michael.gould@pcc.edu>
2022-10-23 19:28:32 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,5 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 
 /**
  * created by Michael Gould 8.27.2022
@@ -6,5 +7,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, world!");
+        System.out.println(new Date());
     }
 }
